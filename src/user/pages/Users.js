d488d394cd2fb6ddb33f00b4dc4a47fa8d1b41da
remedialchaostheory <remedialchaostheory@users.noreply.github.com>
2020-02-10 20:04:20 -0800

    Add Users page

--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -1,7 +1,29 @@
 import React from "react";
 
+import UsersList from "../components/UsersList";
+
 const Users = () => {
-  return <h2>Users worksssss</h2>;
+  const USERS = [
+    {
+      id: 1,
+      name: "Sally Mae",
+      image:
+        "https://cdn.pixabay.com/photo/2020/02/05/15/19/zoo-4821484_1280.jpg",
+      places: 2
+    },
+    {
+      id: 2,
+      name: "Lenny Kite",
+      image:
+        "https://cdn.pixabay.com/photo/2015/06/21/20/12/peacock-816981_1280.jpg",
+      places: 4
+    }
+  ];
+  return (
+    <div>
+      <UsersList items={USERS} />
+    </div>
+  );
 };
 
 export default Users;
