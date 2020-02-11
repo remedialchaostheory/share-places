55e8b1ccba81d4121ac1ea8a9c55685efd8ac7ee
remedialchaostheory <remedialchaostheory@users.noreply.github.com>
2020-02-10 20:17:16 -0800

    Change 'places' to 'placeCount'

--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -9,14 +9,14 @@ const Users = () => {
       name: "Sally Mae",
       image:
         "https://cdn.pixabay.com/photo/2020/02/05/15/19/zoo-4821484_1280.jpg",
-      places: 2
+      placeCount: 2
     },
     {
       id: 2,
       name: "Lenny Kite",
       image:
         "https://cdn.pixabay.com/photo/2015/06/21/20/12/peacock-816981_1280.jpg",
-      places: 4
+      placeCount: 4
     }
   ];
   return (
